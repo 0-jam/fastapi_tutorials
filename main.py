210b7bd69319a2e20d5f4247c9225830b3990513
0-jam <28038981+0-jam@users.noreply.github.com>
2021-03-09 18:04:41 +0900

    Added some description

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+# Generate a FastAPI instance
 app = FastAPI()
 
 
+# Define a path to access this instance
+# 'get' means HTTP GET
+# and execute the function at the next line
 @app.get('/')
 async def root():
     return {'message': 'Hello world'}
