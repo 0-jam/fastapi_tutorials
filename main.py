1b09eb3524fa2dead28aa03ae9a68b2d13de6a46
0-jam <28038981+0-jam@users.noreply.github.com>
2021-03-09 18:35:46 +0900

    The basic path parameters

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'Hello world'}
+
+
+# Pass 'item_id' as arguments of the function
+@app.get('/items/{item_id}')
+async def read_item(item_id):
+    return {'item_id': item_id}
