c8f58220b2ff6c58b457ff9ac9d844071855233f
0-jam <28038981+0-jam@users.noreply.github.com>
2021-03-10 11:46:08 +0900

    Acceptable parameters as enum

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from enum import Enum
+
+
+# Set acceptable values as the subclass of str and Enum
+class ModelName(str, Enum):
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
+
 
 # Generate a FastAPI instance
 app = FastAPI()
@@ -19,6 +28,7 @@ async def read_item(item_id: int):
 
 
 # A fixed path example
+# A fixed path must be declared before other variable paths
 @app.get('/users/me')
 async def read_user_me():
     return {'user_id': 'the current user'}
@@ -27,3 +37,15 @@ async def read_user_me():
 @app.get('/users/{user_id}')
 async def read_user(user_id: str):
     return {'user_id': user_id}
+
+
+# Accept values that is declared above
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {'model_name': model_name, 'message': 'Deep Learning FTW!'}
+
+    if model_name.value == 'lenet':
+        return {'model_name': model_name, 'message': 'LeCNN all the images'}
+
+    return {'model_name': model_name, 'message': 'Have some residuals'}
