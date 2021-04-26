37a7d531a4654a8f1a214a76ac8d3aec930af9d3
0-jam <28038981+0-jam@users.noreply.github.com>
2021-04-26 16:07:45 +0900

    Fixed query parameters using regular expressions

--- items.py
+++ items.py
@@ -7,7 +7,21 @@ app = FastAPI()
 
 # Recieve the parameter 'q' with the default value (None) and the limit of characters (5)
 @app.get('/items/')
-async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=5)):
+async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=50)):
+    results = {'items': [
+        {'item_id': 'Foo'},
+        {'item_id': 'Bar'},
+    ]}
+
+    if q:
+        results.update({'q': q})
+
+    return results
+
+
+# Recieve the parameter 'q' which accepts only the fixed value ('fixedquery') using reqular expressions
+@app.get('/items_fixed/')
+async def read_items_fixed(q: Optional[str] = Query(None, min_length=3, max_length=50, regex='^fixedquery$')):
     results = {'items': [
         {'item_id': 'Foo'},
         {'item_id': 'Bar'},
