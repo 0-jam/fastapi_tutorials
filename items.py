2b80b787a61c3ab3cb504c6cbcbb5159ea4b2a77
0-jam <28038981+0-jam@users.noreply.github.com>
2021-04-26 16:26:58 +0900

    Fixed limit of characters

--- items.py
+++ items.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 # Recieve the parameter 'q' with the default value (None) and the limit of characters (5)
 @app.get('/items/')
-async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=50)):
+async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=5)):
     results = {'items': [
         {'item_id': 'Foo'},
         {'item_id': 'Bar'},
@@ -35,7 +35,7 @@ async def read_items_fixed(q: Optional[str] = Query(None, min_length=3, max_leng
 
 # Recieve the parameter 'q' which accepts only the fixed value ('fixedquery') using reqular expressions
 @app.get('/items_required/')
-async def read_items_required(q: str = Query(..., min_length=3)):
+async def read_items_required(q: str = Query(..., min_length=3, max_length=5)):
     results = {'items': [
         {'item_id': 'Foo'},
         {'item_id': 'Bar'},
