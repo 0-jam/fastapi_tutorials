4f41415d62c2a305f4dc5aede7a529417547c8b2
0-jam <28038981+0-jam@users.noreply.github.com>
2021-04-27 16:34:58 +0900

    The sample for multiple body parameters

--- models.py
+++ models.py
@@ -4,12 +4,17 @@ from fastapi import FastAPI, Path
 from pydantic import BaseModel
 
 
-# Create the data model
+# Create the data model with default values
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
-    tax: Optional[float] = None
+    tax: Optional[float] = 0.1
+
+
+class User(BaseModel):
+    name: str
+    full_name: Optional[str] = None
 
 
 app = FastAPI()
@@ -34,6 +39,7 @@ async def update_item(
     item_id: int = Path(..., title='The ID of the item to get', ge=0, le=100),
     q: Optional[str] = None,
     item: Optional[Item] = None,
+    user: Optional[User] = None,
 ):
     results = {'item_id': item_id}
 
@@ -41,6 +47,6 @@ async def update_item(
         results.update({'q': q})
 
     if item:
-        results.update({'item': item})
+        results.update({'item': item, 'user': user})
 
     return results
